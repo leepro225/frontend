2b9ef290070f9cf0e6898f1bd56f06675f767c4a
ha-co <leepro225@gmail.com>
2019-11-08 22:38:35 +0900

    20191108

--- test.js
+++ test.js
@@ -1,70 +1,100 @@
-// XXX 질문있어요
 
-// FIXME 픽스미 코맨트 테스트
-function sendRequest() {
-    const httpRequest = new XMLHttpRequest();
-
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200 ) {
-
-            const result  = JSON.parse(httpRequest.responseText),
-                  app	  = document.getElementById("app");
-
-            for (let i = 0; i < result.length; i++) {
-                let htmlView = `<div class='Nnq7C weEfm'>
-                                    <div class='v1Nh3 kIKUG _bz0w'>
-                                        <a href='javascript:;'>
-                                        <div class='eLAPa'>
-                                            <div class='KL4Bh'>
-                                                <img class='FFVAD' decoding='auto' src='${result[i]["src1"]}'>
-                                            </div>
-                                            <div class='_9AhH0'>
-                                            </div>
-                                        </div>
-                                        <div class='u7YqG'>
-                                            <span aria-label='슬라이드' class='mediatypesSpriteCarousel__filled__32 u-__7'></span>
-                                        </div>
-                                        </a>
-                                    </div>
-                                    <div class='v1Nh3 kIKUG _bz0w'>
-                                        <a href='javascript:;'>
-                                        <div class='eLAPa'>
-                                            <div class='KL4Bh'>
-                                                <img class='FFVAD' decoding='auto' src='${result[i]["src2"]}'>
-                                            </div>
-                                            <div class='_9AhH0'>
-                                            </div>
-                                        </div>
-                                        <div class='u7YqG'>
-                                            <span aria-label='슬라이드' class='mediatypesSpriteCarousel__filled__32 u-__7'></span>
-                                        </div>
-                                        </a>
-                                    </div>
-                                    <div class='v1Nh3 kIKUG _bz0w'>
-                                        <a href='javascript:;'>
-                                        <div class='eLAPa'>
-                                            <div class='KL4Bh'>
-                                                <img class='FFVAD' decoding='auto' src='${result[i]["src3"]}'>
-                                            </div>
-                                            <div class='_9AhH0'>
-                                            </div>
-                                        </div>
-                                        <div class='u7YqG'>
-                                            <span aria-label='슬라이드' class='mediatypesSpriteCarousel__filled__32 u-__7'></span>
-                                        </div>
-                                        </a>
-                                    </div>		
-                                </div>`;	
-                
-                app.innerHTML += htmlView;
-            }
-
-        }
-    };
+const module = (() => {
     
-    httpRequest.open("GET", "https://my-json-server.typicode.com/it-crafts/mockapi/timeline", true);
-    httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    httpRequest.send();
-}
+    const model = {
+        // 데이터를 요청하는 영역  
+        
+        getData : async function() {
+            let res = await axios.get('https://my-json-server.typicode.com/it-crafts/mockapi/timeline');
+        
+            return res;
+        } 
+        
+    }
 
-sendRequest();		
+    const view = {
+        // html을 그리는 영역
+
+        render : function(data) {
+            const that = this;
+            const app = document.getElementById("app");
+            let html = '';
+            const template = `<div class='Nnq7C weEfm'>
+                                <div class='v1Nh3 kIKUG _bz0w'>
+                                    <a href='javascript:;'>
+                                    <div class='eLAPa'>
+                                        <div class='KL4Bh'>
+                                            <img class='FFVAD' decoding='auto' src='{{src1}}'>
+                                        </div>
+                                        <div class='_9AhH0'>
+                                        </div>
+                                    </div>
+                                    <div class='u7YqG'>
+                                        <span aria-label='슬라이드' class='mediatypesSpriteCarousel__filled__32 u-__7'></span>
+                                    </div>
+                                    </a>
+                                </div>
+                                <div class='v1Nh3 kIKUG _bz0w'>
+                                    <a href='javascript:;'>
+                                    <div class='eLAPa'>
+                                        <div class='KL4Bh'>
+                                            <img class='FFVAD' decoding='auto' src='{{src2}}'>
+                                        </div>
+                                        <div class='_9AhH0'>
+                                        </div>
+                                    </div>
+                                    <div class='u7YqG'>
+                                        <span aria-label='슬라이드' class='mediatypesSpriteCarousel__filled__32 u-__7'></span>
+                                    </div>
+                                    </a>
+                                </div>
+                                <div class='v1Nh3 kIKUG _bz0w'>
+                                    <a href='javascript:;'>
+                                    <div class='eLAPa'>
+                                        <div class='KL4Bh'>
+                                            <img class='FFVAD' decoding='auto' src='{{src3}}'>
+                                        </div>
+                                        <div class='_9AhH0'>
+                                        </div>
+                                    </div>
+                                    <div class='u7YqG'>
+                                        <span aria-label='슬라이드' class='mediatypesSpriteCarousel__filled__32 u-__7'></span>
+                                    </div>
+                                    </a>
+                                </div>		
+                            </div>`;
+
+            data.forEach(function() {
+                htmlView = that.engine(template).render(data);
+            
+                html += htmlView;
+            });
+
+            app.innerHTML = html;
+        },
+
+        engine : function(template) {
+            engine[selector] = template;
+        
+            return {
+                render(data) {
+                    let html = '';
+                    data.forEach(data => {
+                        html += engine[selector].replace(/{{ *(\w+) *}}/g, (m, key) => data[key]);
+                    })
+                    return html;
+                }
+            }
+        }
+    }
+
+    const controller = {
+        // 그 이외의 영역
+        
+        execute : model.getData().then(function (res) {
+            view.render(res.data);
+        })
+
+    }
+    
+})();
