4c02ebbab228c0c054de686ac0ec36a6986fbf05
ha-co <leepro225@gmail.com>
2019-11-15 18:26:59 +0900

    클릭이벤트 추가

--- test.js
+++ test.js
@@ -1,21 +1,22 @@
+const _loading = document.querySelector('._4emnV');
+const selector = "#app";
+const app = document.querySelector(selector);
+let appHeight = document.querySelector(selector);
+const template = `<div class="Nnq7C weEfm"><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;"><div class="eLAPa">
+                    <div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{src1}}" style="object-fit: cover;"></div>
+                    <div class="_9AhH0"></div></div><div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
+                    <div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{  src2 }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
+                    <div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div>
+                    <div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
+                    <div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{ src3  }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
+                    <div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div></div>`;
+
+const url = 'https://my-json-server.typicode.com/it-crafts/mockapi/timeline/';
+const urlInfo = url + "info";
+
 const module = (async() => {
-    const _loading = document.querySelector('._4emnV');
-    const selector = "#app";
-    const app = document.querySelector(selector);
-    let appHeight = document.querySelector(selector);
-    const template = `<div class="Nnq7C weEfm"><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;"><div class="eLAPa">
-                        <div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{src1}}" style="object-fit: cover;"></div>
-                        <div class="_9AhH0"></div></div><div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
-                        <div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{  src2 }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
-                        <div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div>
-                        <div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
-                        <div class="eLAPa"><div class="KL4Bh"><img class="FFVAD" decoding="auto" src="{{ src3  }}" style="object-fit: cover;"></div><div class="_9AhH0"></div></div>
-                        <div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div></div>`;
     
-    const url = 'https://my-json-server.typicode.com/it-crafts/mockapi/timeline/';
-    const urlInfo = url + "info";
-    const timelineInfo = await axios(urlInfo);
-    
+    const timelineInfo = await axios(urlInfo);   
     let page = 1;
     const totalPage = timelineInfo.data.data.totalPage;
 
@@ -77,8 +78,40 @@ const module = (async() => {
         }
 
         window.addEventListener('scroll', scrollEvent);
+  
+        document.querySelectorAll('.fx7hk > a').forEach(tabButton => {
+            tabButton.addEventListener('click', async function(e) {
+                
+                // 컬러를 변경한다.
+                let _9VEo1 = document.getElementsByClassName("_9VEo1");
+                
+                for (let i = 0; i < _9VEo1.length; i++) {
+                    
+                    const _class = _9VEo1[i].childNodes[0].classList[0];
+                    
+                    _class.replace(/blue/gi, 'grey');
 
+                }
+
+                console.log(e);
+
+                // 이전 ajax 요청중인게 있다면 중단한다.
         
+                // 이전 ajax response를 지운다.
+        
+                // app을 비운다.
+        
+                // page를 1로 초기화 한다
+        
+                // TODO 버튼 누를 때는 1페이지로 새로 요청을 해야 함
+                if('' === _loading.style.display) {
+                    return;
+                }
+                _loading.style.display = '';
+                await excute();
+                _loading.style.display = 'none';
+            });
+        });
     };
     
     controller();       
@@ -86,23 +119,6 @@ const module = (async() => {
 })();
 
 
-// TODO 버튼클릭시 동일 API로 처음부터 다시 시작하도록 해주세요
-
-document.querySelectorAll('.fx7hk > a').forEach(tabButton => {
-    tabButton.addEventListener('click', async function(e) {
-        let _9VEo1 = document.getElementsByClassName("_9VEo1");
-        
-        console.dir(_9VEo1);
-
-        // TODO 버튼 누를 때는 1페이지로 새로 요청을 해야 함
-        if('' === _loading.style.display) {
-            return;
-        }
-        _loading.style.display = '';
-        // await timeline.render();
-        _loading.style.display = 'none';
-    });
-});
 
 
 
